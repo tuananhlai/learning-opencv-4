03080019ef984098498e5fe7c5401f19e1cc7b87
anhlt <42168740+tuananhlai@users.noreply.github.com>
2021-01-21 23:05:32 +0700

    add chap6

---
+++ chapter6/create_descriptor.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+import os
+
+def create_descriptors(folder):
+    feature_detector = cv2.xfeature2d.SIFT_create()
+    files = []
+    for (dirpath, dirnames, filenames) in os.walk(folder):
+        files.extend(filenames)
+    for f in files:
+        create_descriptor(folder, f, feature_detector)
+
+def create_descriptor(folder, image_path, feature_detector):
+    if not image_path.endswith('png'):
+        print('skipping %s' % image_path)
+        return
+    print('reading %s' % image_path)
+    img = cv2.imread(os.path.join(folder, image_path, cv2.IMREAD_GRAYSCALE))
+    keypoints, descriptors = feature_detector.detectAndCompute(img, None)
+    descriptor_file = image_path.replace('png', 'npy')
+    np.save(os.path.join(folder, descriptor_file), descriptors)
+
+if __name__ == '__main__':
+    create_descriptors('./tattoo')
